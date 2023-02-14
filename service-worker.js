d7ac732936a2673106e9be9c527cf282db5f6a6a
CsharpAssassin <123393767+CsharpAssassin@users.noreply.github.com>
2023-02-14 11:55:38 +0200

    Update service-worker.js
    Again

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 self.addEventListener('push', function (event) {
     console.log('Push notification received:', event);
 
-    if (event.data) {
+    if (event.data) { 
         // Display the push notification message
         self.registration.showNotification('My Push Notification', {
             body: event.data.text()
